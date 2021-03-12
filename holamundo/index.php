db2d27a3e4ed1d82747fce2fd8d5831d3bd741c2
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-11 21:30:12 -0600

    Valores primarios

--- holamundo/index.php
+++ holamundo/index.php
@@ -1,53 +1,73 @@
+<?php  
+
+// String
+$nombre = 'Jesus';
+
+//integer
+$edad = 32;
+
+// double
+$deciman = 3.2;
+
+//booleans
+$verdaro = true;
+$falso = false;
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
 
-<!-- **************************  -->
-<!-- **************************  -->
+    <!-- **************************  -->
+    <!-- **************************  -->
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="keywords" content="">
-     <meta name="author" content="Jesus Miranda">
-<!-- **************************  -->
-<!-- **************************  -->
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="INDEX,FOLLOW">
-<!-- **************************  -->
-<!-- **************************  -->
+    <!-- **************************  -->
+    <!-- **************************  -->
     <link rel="stylesheet" href="assets/css/all.css">
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/responsivo.css">
     <link rel="stylesheet" href="assets/css/estilos.css">
-<!-- **************************  -->
-<!-- **************************  -->    
-<link rel="icon" type="image/png" href="">
-<!-- **************************  -->
-<!-- **************************  -->
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
     <title></title>
 
-    
+
 </head>
+
 <body>
-    
-<!-- **************************  -->
-<!-- **************************  -->
+
+    <!-- **************************  -->
+    <!-- **************************  -->
     <header>
 
-        <?php echo 'Hola Mundo'; ?>
+        <h1><?php echo 'Hola Mundo, Hola ' . $nombre ; ?></h1>
+
+        <h2> <?php  echo 'Hola soy ' . $nombre . ' Tengo ' . $edad . ' Años'?> </h2>
 
     </header>
 
 
-<!-- **************************  -->
-<!-- **************************  -->
+    <!-- **************************  -->
+    <!-- **************************  -->
     <main>
 
     </main>
 
 
-<!-- **************************  -->
-<!-- **************************  -->
+    <!-- **************************  -->
+    <!-- **************************  -->
     <footer>
 
 
@@ -56,4 +76,5 @@
 
 
 </body>
+
 </html>
