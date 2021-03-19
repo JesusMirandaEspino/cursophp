9c6fe6c0a75a7b6f4f78a6edb394f8a0ad2ed18d
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-18 21:22:20 -0600

    Functions

---
+++ holamundo/app.php
@@ -0,0 +1,90 @@
+<?php     
+
+$texto = 'Jesus';
+$numero = 10;
+$numero2 = '5';
+$arreglo = array( 'Yisus', 'Mutuelo', 'Ramo' );
+$asociativo = array( '1' => 'uno', '2' => 'dos', '3' => 'tres' );
+$booleano = false;
+
+var_dump( $texto );
+print_r( $texto );
+
+
+function saludo(){
+    echo 'Hola desde php';
+}
+
+
+function suma( $a, $b ){
+    $c = $a + $b;
+    echo 'La suma de los valores es ' . $c;
+}
+
+saludo();
+suma( 5, 10 );
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+
+    </header>
+
+
+
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+    </footer>
+</body>
+
+</html>
