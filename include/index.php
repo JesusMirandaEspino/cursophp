173fc37a8177d8a6e68ff6d0fa7c68461742c069
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-23 21:07:27 -0600

    include & require

---
+++ include/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php  
+
+
+require( 'app.php' );
+
+//include('app.php' );
+
+?>
+
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+    <h1> <?php  echo Sumar( 2, 5 ); ?>  </h1>
+
+
+    <h2> Hola a todos </h2>
+
+    </header>
+
+
+
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+    </footer>
+</body>
+
+</html>
