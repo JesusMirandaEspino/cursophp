cedc0dad8855487cd15538f1c2f0b2531328a189
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-23 22:14:07 -0600

    form

---
+++ formulario/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+
+    </header>
+
+
+
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+        <section class="fila">
+
+            <form action="app.php" method="" class="contenedor1">
+
+                <div class="col-full-12">
+                    <label for="nameuser">Nombre</label>
+                    <input type="text" id="nameuser" name="nameuser">
+                </div>
+
+                <div class="col-full-12">
+                    <label for="man">Masculino</label>
+                    <input type="radio" name="sextype" id="man" value="Hombre">
+                    <label for="woman">Femenino</label>
+                    <input type="radio" name="sextype" id="woman" value="Mujer">
+                </div>
+
+                <div class="col-full-12">
+                    <select name="year" id="year">
+                        <option value="2000">2000</option>
+                        <option value="2001">2001</option>
+                        <option value="2002">2002</option>
+                        <option value="2003">2003</option>
+                    </select>
+                </div>
+
+
+                <div class="col-full-12">
+                    <label for="terms">Acepto Terminos y Condiciones</label>
+                    <input type="checkbox" name="terms" id="temrs" value="ok">
+                </div>
+
+
+
+                <div class="col-full-12">
+                    <input type="submit" value="Enviar">
+                </div>
+
+
+
+            </form>
+
+
+        </section>
+
+
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+    </footer>
+</body>
+
+</html>
