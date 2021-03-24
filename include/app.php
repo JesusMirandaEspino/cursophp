7b083012639ec0bbafa99d90b7fff4b93ebab3b1
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-23 21:24:04 -0600

    Escalar type

--- include/app.php
+++ include/app.php
@@ -1,6 +1,18 @@
 <?php
 
+    $valor = '8';
+
 
     function Sumar( $numero1, $numero2 ){
         return $numero1 + $numero2;
     }
+
+
+    function Multiplicar( int $valor  ){
+        return $valor * $valor;
+    }
+
+
+
+
+?>
