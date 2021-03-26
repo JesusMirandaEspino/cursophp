53f0825cacf1e193a0dad4807623910c00b6a513
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-25 22:13:31 -0600

    Practica formulario

---
+++ practicas/formulario/index.view.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+
+    </header>
+
+
+
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+        <section class="fila">
+
+            <form action=" <?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>  " method="post" class="contenedor1">
+
+                <div class="col-full-12">
+                    <label for="nameuser">Nombre</label>
+                    <input type="text" id="nameuser" name="nameuser" placeholder="Nombre">
+                </div>
+
+                <div class="col-full-12">
+                    <label for="emailuser">Correo</label>
+                    <input type="email" id="emailuser" name="emailuser" placeholder="Correo">
+                </div>
+
+                <div class="col-full-12">
+                    <textarea name="menssage" id="menssage" cols="30" rows="10"></textarea>
+                </div>
+
+
+                <div class="col-full-12">
+                    <input type="submit" value="Enviar" name="submit">
+                </div>
+
+
+
+            </form>
+
+
+        </section>
+
+
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+    </footer>
+</body>
+
+</html>
