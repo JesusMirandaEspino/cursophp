bdb31a6ed1f1f97d705d07bd7d03246576a1026f
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-03-25 21:57:24 -0600

    Fusion-null

---
+++ fusion/index.php
@@ -0,0 +1,76 @@
+<?php
+
+    // $nombre =  isset($_GET['nombre'] ) ?  $_GET['nombre'] : 'Anonimo';
+
+    $nombre =  $_GET['nombre'] ?? 'Anonimo';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+
+    </header>
+
+
+
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+        <section class="fila">
+
+        </section>
+
+
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+    </footer>
+</body>
+
+</html>
