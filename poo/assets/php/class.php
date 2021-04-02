810622d904c4a95f1e02ef5e38b263308e2327d3
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-01 20:04:17 -0600

    class & this

--- poo/assets/php/class.php
+++ poo/assets/php/class.php
@@ -5,9 +5,8 @@ class Persona {
     public $edad;
     public $pais;
 
-
     public function mostrarInformacion(){
-        echo 'Hola mundo';
+        echo $this->nombre;
     }
 
 }
@@ -15,15 +14,15 @@ class Persona {
 
 
 $usuario1  = new Persona;
-$usuario1->$nombre = 'Mutuelo';
-$usuario1->$edad = 25000;
-$usuario1->$pais = 'lightstorm';
+$usuario1->nombre = 'Mutuelo';
+$usuario1->edad = 25000;
+$usuario1->pais = 'lightstorm';
 $usuario1->mostrarInformacion();
 
 $usuario2  = new Persona;
-$usuario2->$nombre = 'Maxking';
-$usuario2->$edad = 35000;
-$usuario2->$pais = 'darkvillage';
+$usuario2->nombre = 'Maxking';
+$usuario2->edad = 35000;
+$usuario2->pais = 'darkvillage';
 $usuario2->mostrarInformacion();
 
 ?>
--- poo/index.php
+++ poo/index.php
@@ -1,5 +1,5 @@
 <?php 
-require('/assets/php/class.php');
+require('./assets/php/class.php');
 
 
 ?>
