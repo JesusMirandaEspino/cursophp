ddfb9f769fdf87d2a5de70694ae71fa14c22f1d5
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-01 20:11:01 -0600

    class constructor

--- poo/assets/php/class.php
+++ poo/assets/php/class.php
@@ -5,24 +5,23 @@ class Persona {
     public $edad;
     public $pais;
 
+    public function __construct($nombre, $edad, $pais ){
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+        $this->pais = $pais;
+        
+    }
+
     public function mostrarInformacion(){
         echo $this->nombre;
     }
 
 }
 
-
-
-$usuario1  = new Persona;
-$usuario1->nombre = 'Mutuelo';
-$usuario1->edad = 25000;
-$usuario1->pais = 'lightstorm';
+$usuario1  = new Persona('Mutuelo', 25000, 'lightstorm' );
 $usuario1->mostrarInformacion();
 
-$usuario2  = new Persona;
-$usuario2->nombre = 'Maxking';
-$usuario2->edad = 35000;
-$usuario2->pais = 'darkvillage';
+$usuario2  = new Persona( 'Maxking', 35000, 'darkvillage' );
 $usuario2->mostrarInformacion();
 
 ?>
