3383b7b898f20b8f0274432b79c1ef2b230c03f8
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-08 20:58:44 -0500

    mysql console

---
+++ poo/console.php
@@ -0,0 +1,11 @@
+<?php 
+
+    //accede a la direcion de los archivos de mysql
+    //accede a algun usario con mysql -u usuario
+    //escribe contraseña
+
+
+    echo 'Muestra las bases de datos con SHOW DATABASE';
+    echo 'Muestra las bases de datos con CREATE DATABASE nombre';
+
+?>
