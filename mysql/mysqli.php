2bd5c8b21d2ed4c5585dfb874bf2ef7ed4aed174
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-20 22:08:33 -0500

    conect to fetch assoc

--- mysql/mysqli.php
+++ mysql/mysqli.php
@@ -4,6 +4,22 @@ $conectar = new mysqli( 'localhost', 'root', '', 'prueba_datos' );
 
 if($conectar->connect_errno){
     die();
+}else{
+    $sql = 'SELECT * FROM usuarios';
+    $result = $conectar->query( $sql );
+
+    if($result->num_rows){
+
+        
+        echo '<pre>';
+        var_dump( $result->fetch_assoc() );
+        var_dump( $result->fetch_assoc() );
+        echo '</pre>';
+
+
+    }else{
+        echo 'No hay Datos';
+    }
 }
 
 ?>
