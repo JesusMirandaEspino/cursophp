85ec5ed298d3ec7f60fde8a268fc4d1b165bd994
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-22 22:21:54 -0500

    conect & to show

--- paginacion/index.php
+++ paginacion/index.php
@@ -10,6 +10,34 @@ try{
 }
 
 
+$pagina = isset($_GET['pagina']) ? (int) $_GET['pagina'] : 1;
+$postpagina = 5;
+
+$inicio = ($pagina > 1 ) ? ( $pagina * $postpagina - $postpagina  ) : 0;
+
+$articulos = $conexion->prepare( " SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $postpagina " );
+$articulos->execute();
+
+$articulos = $articulos->fetchAll();
+
+
+//print_r($articulos);
+
+if( !$articulos ){
+    header('Location: index.php');
+}
+
+
+$totalArticulos = $conexion->query( 'SELECT FOUND_ROWS() as total' );
+$totalArticulos = $totalArticulos->fetch()['total'];
+
+//echo $totalArticulos;
+
+
+$numeroPaginas = ceil($totalArticulos / $postpagina);
+
+//echo $numeroPaginas; 
+
 require( 'index.view.php' );
 
 
