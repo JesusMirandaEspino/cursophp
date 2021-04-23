8e74c2f92962bb5154152e6d5595801fa3e27f68
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-22 20:38:46 -0500

    paginacion php

---
+++ paginacion/index.view.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title></title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+        <section class="fila">
+
+            <div class="contenedor1">
+
+                <div class="col-full-12">
+
+                    <h1> Articulos </h1>
+
+                </div>
+
+            </div>
+
+        </section>
+
+
+
+    </header>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+        <section class="fila  articulos ">
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <ul>
+                        <li>1.- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>2.- Nam quis nibh porta, elementum mauris quis.</li>
+                        <li>3.- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>4.- Nam quis nibh porta, elementum mauris quis.</li>
+                        <li>5.- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="fila">
+
+            <div class="contenedor1">
+
+                <div class="col-full-12">
+
+                    <ul>
+                        <li class="disabled"><a href="">&laquo;</a></li>
+                        <li class="active"><a href="">1</a></li>
+                        <li><a href="">2</a></li>
+                        <li><a href="">3</a></li>
+                        <li><a href="">4</a></li>
+                        <li><a href="">&raquo;</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+
+    </footer>
+
+
+
+</body>
+
+</html>
