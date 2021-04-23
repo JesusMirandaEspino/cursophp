2915809ebaaf7b509a377c256b336a7a9bf14e4d
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-22 21:53:46 -0500

    paginacion db conect

--- paginacion/index.php
+++ paginacion/index.php
@@ -1,6 +1,15 @@
 <?php
 
 
+try{
+    $conexion = new PDO( 'mysql:host=localhost;dbname=paginacion', 'root', '');
+
+}catch(  PDOException $e ){
+    echo 'Error' .$e->getMessage(); 
+    die();
+}
+
+
 require( 'index.view.php' );
 
 
