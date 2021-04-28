183e355c3a1d85e604f2f15f015787ec3797bedb
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-27 22:38:07 -0500

    Paginación completa

--- paginacion/index.view.php
+++ paginacion/index.view.php
@@ -80,12 +80,38 @@
                 <div class="col-full-12">
 
                     <ul class="btn-list">
-                        <li class="disabled btn-page"><a href="">&laquo;</a></li>
-                        <li class="active btn-page"><a href="">1</a></li>
+
+                        <?php if ($pagina == 1) :  ?>
+                            <li class="disabled btn-page"><a href="">&laquo;</a></li>
+                        <?php else : ?>
+                            <li class="btn-page"><a href="?pagina=<?php echo $pagina - 1; ?>">&laquo;</a></li>
+                        <?php endif; ?>
+
+
+                        <?php
+                        for ($i = 1; $i <= $numeroPaginas; $i++) {
+                            if ($pagina == $i) {
+                                echo "<li class='active btn-page'><a href='?pagina=$i'>$i</a></li>";
+                            } else {
+                                echo "<li class='btn-page'><a href='?pagina=$i'>$i</a></li>";
+                            }
+                        }
+                        ?>
+
+
+                        <?php if ($pagina == $numeroPaginas) :  ?>
+                            <li class="disabled btn-page"><a href="">&raquo;</a></li>
+                        <?php else : ?>
+                            <li class="btn-page"><a href="?pagina=<?php echo $pagina + 1; ?>">&raquo;</a></li>
+                        <?php endif; ?>
+
+
+
+                        <!--
                         <li class="btn-page"><a href="">2</a></li>
                         <li class="btn-page"><a href="">3</a></li>
                         <li class="btn-page"><a href="">4</a></li>
-                        <li class="btn-page"><a href="">&raquo;</a></li>
+                        <li class="btn-page"><a href="">&raquo;</a></li>-->
                     </ul>
 
                 </div>
