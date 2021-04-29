38aca1379e62f6442a0dfaba49825892c5ed9a7b
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-04-29 18:00:30 -0500

    font awasome

--- registro/index.php
+++ registro/index.php
@@ -15,17 +15,17 @@
     <meta charset="UTF-8">
     <meta name="description" content="">
     <meta name="keywords" content="">
-     <meta name="author" content="Jesus Miranda">
+    <meta name="author" content="Jesus Miranda">
 <!-- **************************  -->
 <!-- **************************  -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="INDEX,FOLLOW">
 <!-- **************************  -->
 <!-- **************************  -->
-    <link rel="stylesheet" href="assets/css/all.css">
-    <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/responsivo.css">
-    <link rel="stylesheet" href="assets/css/estilos.css">
+    <link rel="stylesheet" href="/assets/css/all.css">
+    <link rel="stylesheet" href="/assets/css/reset.css">
+    <link rel="stylesheet" href="/assets/css/responsivo.css">
+    <link rel="stylesheet" href="/assets/css/estilos.css">
 <!-- **************************  -->
 <!-- **************************  -->    
 <link rel="icon" type="image/png" href="">
