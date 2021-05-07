e7d22ef429ea3a22d9f6759466c01881f06fab46
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-06 22:10:11 -0500

    practica registro encriptar contraseña

--- registro/registrate.php
+++ registro/registrate.php
@@ -12,31 +12,42 @@
         $password2 = $_POST['password2'];
 
     
-        echo $usuario .  $password . $password2;
+        
 
 
         $errores = '';
+        $falso = false;
 
         if( empty( $usuario ) || empty($password) || empty($password2) ){
             $errores .= '<li> Por favor rellena los datos correctamente  </li>';
         }else{
             try{
 
-                $conexion = new PDO( 'mysql:host=localhost; dbname:login', 'root', ''  );
+                $conexion = new PDO( 'mysql:host=localhost; dbname=login', 'root', ''  );
 
             }catch( PDOException $e ){
                 echo "Error"  . $e->getMessage();
             }
 
             $statement = $conexion->prepare( ' SELECT * FROM usuarios WHERE usuario = :usuario LIMIT 1  ' );
-            $statement->execute( array( ':usuario = $usuario' ) );
+            $statement->execute( array( ':usuario' => $usuario ) );
             $resultado = $statement->fetch();
 
-            if( !$resultado ){ // !$resultado
+
+            if( $resultado != $falso ){ // !$resultado
+            
                     $errores .= '<li> El usuario ya existe  </li>';
             }
 
+            $password  = hash( 'sha512', $password );
+            $password2  = hash('sha512', $password2 );
+
+
+
         }
+
+
+    echo $usuario .  $password . $password2;
 
 
     }
