5088c3e7eda800d77f98e839cc4b0b8a107fe0e2
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-11 22:20:50 -0500

    practica registro login

--- registro/login.php
+++ registro/login.php
@@ -1,5 +1,19 @@
 <?php
 
+if ($_SESSION['usuario']) {
+    header('Location: index.php');
+} 
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
+    $usuario = filter_var(  strtolower( $_POST['usuario'] ), FILTER_SANITIZE_STRING );
+    $password = $_POST['password'];
+    $password = hash( 'sha512', $password );
+
+    
+}
+
+
+
 require('views/login.view.php');
 
 
