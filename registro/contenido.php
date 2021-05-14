7ef326b6e8f7368286b31cea590fae1b0047323c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-13 20:37:20 -0500

    practica registro protect content

--- registro/contenido.php
+++ registro/contenido.php
@@ -1,5 +1,13 @@
 <?php 
 
-    require('views/contenido.view.php');
+    session_start();
+
+    if( isset( $_SESSION['usuario'] )){
+        require('views/contenido.view.php');
+    }else{
+        header('Location: login.php');
+    }
+
+    
 
 ?>
