db20d8c575333dab15f6cbcdfeb30a5482ad305a
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-13 21:15:56 -0500

    practica modificar archivo

--- crear/archivos.php
+++ crear/archivos.php
@@ -8,6 +8,21 @@
     }
 */
 
-echo file_get_contents('documento.txt');
+// file_put_contents('documento.txt', 'Hola jesus', FILE_APPEND );
+
+
+
+
+    for( $i = 0; $i <= 10; $i++ ){
+        file_put_contents('documento.txt', "$i " , FILE_APPEND);  
+    }
+
+
+// echo file_get_contents('documento.txt');
+
+
+$archivo = file( 'documento.txt' );
+    echo '<pre>';
+    print_r($archivo);
 
 ?>
