46cf5c1a34f2939b2306329138acc2235fea6bc8
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-13 20:58:38 -0500

    practica archivo file content

---
+++ crear/archivos.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/*   if( file_exists('documento.txt') ){
+        echo  'El archivo si existe';
+    }else{
+        echo  'El archivo no existe';
+    }
+*/
+
+echo file_get_contents('documento.txt');
+
+?>
