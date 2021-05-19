0127e4f73973a62af25f9d3e893ef44b57c73ea1
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-18 22:10:42 -0500

    practica galería foto

---
+++ galeria/subir.php
@@ -0,0 +1,3 @@
+<?php
+    require('views/subir.views.php')
+?>
---
+++ galeria/views/foto.views.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/responsivo.css">
+    <link rel="stylesheet" href="assets/css/estilos.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title>Galeria</title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+
+        <section class="fila">
+
+            <div class="contenedor1">
+
+
+                <div class="col-full-12">
+
+                    <h1>Galeria. Usando PHP y Mysql</h1>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </header>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+        <section class="fila">
+
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <h2>Foto</h2>
+                </div>
+            </div>
+
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <img src="" alt="">
+                </div>
+                <div class="col-full-12">
+                    <p>Lorem </p>
+                </div>
+                <div class="col-full-12">
+                    <a href=""><i class="fas fa-arrow-left"></i> Regresar </a>
+                </div>
+            </div>
+
+        </section>
+
+
+    </main>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <footer>
+
+
+    </footer>
+
+
+
+</body>
+
+</html>
