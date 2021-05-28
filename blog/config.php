20654ef4c5311d82ea55351d5895b5ed19558512
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-27 23:09:24 -0500

    practica blog header

---
+++ blog/config.php
@@ -0,0 +1,23 @@
+<?php
+
+    define('RUTA', 'http://localhost/cursophp/blog/'  );
+
+    $bd_config = array(
+        'bd' => 'blog',
+        'usuario' => 'root',
+        'pass' => ''
+    );
+
+
+    $blog_config = array(
+        'post_por_pagina' => '2',
+        'carpeta_imagenes' => '/assets/update/',
+    );
+
+    $blog_admin = array(
+        'user' => 'jesus',
+        'password' => '1234',
+    );
+
+
+?>
---
+++ blog/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require('config.php');
+
+require('views/index.view.php');
+
+
+
+?>
