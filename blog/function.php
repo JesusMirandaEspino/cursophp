6612fac785c535d9f8e4cab53a6223b7f44fab37
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-03 21:26:43 -0500

    plarctica blog post & right connect

--- blog/function.php
+++ blog/function.php
@@ -24,9 +24,12 @@ function pagina_actual(){
     return isset( $_GET['p'] ) ? (int)$_GET['p'] : 1;
 }
 
-function obtenerPost( $post_por__pagina, $conexion )
+function obtenerPost( $post_por_pagina, $conexion )
 {
     $inicio = ( pagina_actual() > 1 ) ? pagina_actual() * $post_por_pagina - $post_por_pagina : 0; 
+    $sentencia = $conexion->prepare( "SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $post_por_pagina" );
+    $sentencia->execute();
+    return   $sentencia->fetchAll();
 
 }
 
--- blog/index.php
+++ blog/index.php
@@ -5,11 +5,15 @@ require('config.php');
 require('function.php');
 
 
-conexion($bd_config);
+$conexion = conexion($bd_config);
 
 if( !$conexion ){
     header('Location: error.php');
 }
+
+$post = obtenerPost($blog_config['post_por_pagina'], $conexion);
+
+
 
 require('views/index.view.php');
 
