f12f712a749defca608f2bf66bcbbcd9bd49683e
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-22 20:56:15 -0500

    practica blog edtar

--- blog/admin/editar.php
+++ blog/admin/editar.php
@@ -1,6 +1,73 @@
 <?php
 
+session_start();
 
-echo 'editar';
+require('../config.php');
+require('../function.php');
+
+
+$conexion = conexion($bd_config);
+
+comprobarSesion();
+
+if (!$conexion) {
+    header('Location: ../error.php');
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $titulo = limpiardatos($_POST['titulo']);
+    $extracto = limpiardatos($_POST['extracto']);
+    $texto = $_POST['titulo'];
+    $id = limpiardatos($_POST['id']);
+    $thumb_guardada = $_FILES['thumb-guardada'];
+    $thumb = $_FILES['thumb'];
+
+    if( empty( $thumb['name'] ) ){
+        $thumb = $thumb_guardada;
+    }else{
+        $archivo_subido = '../' . $blog_config['carpeta_imagenes'] . $_FILES['thumb']['name'];
+        move_uploaded_file( $_FILES['thumb']['tmp_name'], $archivo_subido );
+        $thumb = $_FILES['thumb']['name'];
+    }
+
+
+
+    $statement = $conexion->prepare(' INSERT  articulos  SET  titulo = :titulo,  extracto = :extracto,  texto = :texto,  thumb = :thumb   WHERE id = :id ');
+
+    $statement->execute(array(
+        ':titulo' => $titulo,
+        ':extracto' => $extracto,
+        ':texto' => $texto,
+        ':thumb' => $thumb,
+        ':id'  =>  $id
+    ));
+
+    header('Location: http://localhost/cursophp/blog/admin');
+
+
+}else{
+
+    $id_articulo = id_articulo( $_GET['id'] );
+
+    if( empty($id_articulo) ){
+        header('Location: http://localhost/cursophp/blog/admin');
+    }
+
+
+    $post = obtener_post_por_id( $conexion, $id_articulo );
+
+    if( !$post  ){
+        header('Location: http://localhost/cursophp/blog/admin');
+    }
+
+    $post = $post[0];
+
+}
+
+
+require('../views/editar.view.php');
+
 
 ?>
---
+++ blog/views/editar.view.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Curso de php blog">
+    <meta name="keywords" content="">
+    <meta name="author" content="Jesus Miranda">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/all.css">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/responsivo.css">
+    <link rel="stylesheet" href="../assets/css/estilos.css">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <link rel="icon" type="image/png" href="">
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <title>Blog</title>
+
+
+</head>
+
+<body>
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <header>
+
+        <section class="fila">
+
+            <div class="contenedor1">
+
+                <div class="col-full-4">
+
+                    <h1><a href="<?php echo 'http://localhost/cursophp/blog/'; ?>">Mi Blog</a></h1>
+
+                </div>
+
+                <div class="col-full-4">
+                    <form action="<?php echo 'http://localhost/cursophp/blog/';  ?>/buscar.php" method="get">
+                        <input type="text" name="busqueda" id="" placeholder="Buscar">
+                        <button type="submit"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+
+                <div class="col-full-4">
+
+                    <nav>
+
+                        <ul>
+                            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="">Contacto <i class="far fa-envelope"></i> </a></li>
+                        </ul>
+
+                    </nav>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </header>
+
+
+    <!-- **************************  -->
+    <!-- **************************  -->
+    <main>
+
+
+        <form enctype="multipart/form-data" method="post" action="<?php echo   htmlspecialchars($_SERVER['PHP_SELF']); ?>" class=" fila">
+
+            <div class="contenedor1">
+
+
+                <div class="col-full-12">
+                    <label for="titulo">Titulo</label>
+                </div>
+
+
+                <div class="col-full-12">
+                    <input type="hidden" name="id" id="id" value=" <?php echo $post['id']  ?> ">
+                </div>
+
+                <div class="col-full-12">
+                    <input type="text" name="titulo" id="titulo" value=" <?php echo $post['titulo']  ?> ">
+                </div>
+
+            </div>
+
+
+            <div class="contenedor1">
+
+                <div class="col-full-12">
+                    <label for="extracto">Extracto de articulo</label>
+                </div>
+
+                <div class="col-full-12">
+                    <input type="text" name="extracto" id="extracto" value=" <?php echo $post['extracto']  ?> ">
+                </div>
+
+
+            </div>
+
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <textarea name="texto" id="texto">  <?php echo $post['texto']  ?>   </textarea>
+                </div>
+            </div>
+
+
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <input type="file" name="thumb" id="thumb">
+                </div>
+
+                <div class="col-full-12">
+                    <input type="hidden" name="thumb-guardada" id="thumb-guardada" value=" <?php echo $post['thumb']  ?> ">
+                </div>
+            </div>
+
+            <div class="contenedor1">
+                <div class="col-full-12">
+                    <input type="submit" value="Editar Articulo">
+                </div>
+            </div>
+
+        </form>
+
+
+
+
+
+    </main>
+
+
+
+
+
+    <?php require('footer.php'); ?>
