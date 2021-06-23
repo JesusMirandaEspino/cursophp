6f020a65d1e4b215d713abd658eb109ab9a7fe62
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-22 21:07:48 -0500

    practica blog delete post

--- blog/admin/borrar.php
+++ blog/admin/borrar.php
@@ -1,8 +1,30 @@
 <?php
 
+session_start();
+
+require('../config.php');
+require('../function.php');
 
 
-    echo 'borrar';
+$conexion = conexion($bd_config);
+
+comprobarSesion();
+
+if (!$conexion) {
+    header('Location: ../error.php');
+}
+
+
+
+$id = limpiardatos($_GET['id']);
+
+if( !$id ){
+    header('Location: http://localhost/cursophp/blog/admin');
+}
+
+$statement = $conexion->prepare(' DELETE FROM articulos WHERE id = :id ');
+$statement->execute( array( 'id'  => $id ) );
+header('Location: http://localhost/cursophp/blog/admin');
 
 
 ?>
