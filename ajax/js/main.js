70ef5de815e5c46df18525edafc6047a81e1315a
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-24 21:10:47 -0500

    practica ajax get data

--- ajax/js/main.js
+++ ajax/js/main.js
@@ -9,6 +9,22 @@ btn.addEventListener( 'click', () => {
 
     loader.classList.add('active');
 
+    peticion.onload = () => {
+        const datos = JSON.parse( peticion.response );
+
+        datos.forEach( persona  => {
+            const elemento = document.createElement('tr');
+            elemento.innerHTML += '<td>' +  persona.id + '</td>' ;
+            elemento.innerHTML += '<td>' +  persona.nombre + '</td>' ;
+            elemento.innerHTML += '<td>' +  persona.edad + '</td>' ;
+            elemento.innerHTML += '<td>' +  persona.pais + '</td>' ;
+            elemento.innerHTML += '<td>' +  persona.correo + '</td>' ;
+            document.querySelector('#tabla').appendChild(elemento);
+
+        } )
+        
+    }
+
 
     peticion.onreadystatechange = () => {
         if( peticion.readyState == 4 && peticion.status == 200){
