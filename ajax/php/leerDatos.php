2d8a6ae9083343689dd6a78b86dd016f68600cde
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-29 21:06:27 -0500

    practica ajax conectar db

---
+++ ajax/php/leerDatos.php
@@ -0,0 +1,43 @@
+<?php
+
+error_reporting(0);
+
+
+header('Content-type: application/json, charset= utf-8');
+
+function conexion()
+{
+
+    try {
+        $conexion = new PDO('mysql:host=localhost;dbname=ajax', 'root', '');
+        $statement = $conexion->prepare('SELECT * FROM usuarios');
+        $statement->execute();
+        $resultados = $statement->fetchAll();
+
+        $respuesta = [];
+
+        foreach ($resultados as $res) {
+            $usuario = [
+                'id'        => $res['id'],
+                'nombre'    => $res['nombre'],
+                'edad'      => $res['edad'],
+                'pais'      => $res['pais'],
+                'correo'    => $res['correo']
+            ];
+
+            array_push($respuesta, $usuario);
+        }
+
+        echo json_encode($respuesta);
+    } catch (PDOException $e) {
+        $respuesta = ['Error' => true];
+        echo 'Error ' . $e;
+        die();
+    }
+}
+
+
+conexion();
+
+
+?>
