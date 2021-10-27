fd3550a07b9d9951efcfca64b3c7d48811605f24
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-26 20:12:04 -0500

    parte 5

---
+++ parte5/assets/php/connect.php
@@ -0,0 +1,25 @@
+<?php
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $port = '3306';
+    $db = 'quiz';
+
+
+    try{
+
+        $conn = new PDO('mysql:host=localhost;dbname=quiz', $user, $pass);
+        echo 'conexion existosa' . '<br/>';
+        
+
+    }catch(PDOException $e){
+
+        echo 'Error al conectar a la base de datos' . $e->getMessage();
+
+    }
+
+
+
+
+?>
