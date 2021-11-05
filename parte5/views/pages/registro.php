f98249d9c9eefa2b295b31ff2d3b999e28b022bb
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-11-04 20:42:12 -0600

    registro styles

--- parte5/views/pages/registro.php
+++ parte5/views/pages/registro.php
@@ -3,19 +3,39 @@
 </div>
 
 
-<form>
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="d-flex" >
+
+    <form class="bg-light ">
+        <div class="mb-3">
+            <label for="nombre" class="form-label">Nombre</label>
+            <div class="input-group">
+                <div class="input-group-pretend">
+                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                </div>
+                <input type="text" class="form-control" id="nombre" aria-describedby="nombre">
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="email" class="form-label">Correo Electronico</label>
+            <div class="input-group">
+                <div class="input-group-pretend">
+                    <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                </div>
+                <input type="email" class="form-control" id="email" aria-describedby="email">
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="pwd" class="form-label">Password</label>
+            <div class="input-group">
+                <div class="input-group-pretend">
+                    <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                </div>
+                <input type="password" class="form-control" id="pwd" aria-describedby="pwd">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+</div>
