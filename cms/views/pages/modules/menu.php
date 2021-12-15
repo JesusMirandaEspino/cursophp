df635b9763b3256706508bb3201c4abb54b564b7
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-12-14 20:36:57 -0600

    update construction cms

--- cms/views/pages/modules/menu.php
+++ cms/views/pages/modules/menu.php
@@ -1,57 +1,4 @@
-<!--=====================================
-BANNER
-======================================-->
 
-<div class="bannerEstatico d-none d-md-block"></div>
-
-<section class="jd-slider fade-slider">
-
-    <div class="slide-inner">
-
-        <ul class="slide-area">
-
-            <li>
-
-                <img src="./views/img/banner01.jpg" alt="img" class="img-fluid">
-
-            </li>
-
-            <li>
-
-                <img src="./views/img/banner02.jpg" alt="img" class="img-fluid">
-
-            </li>
-
-            <li>
-
-                <img src="./views/img/banner03.jpg" alt="img" class="img-fluid">
-
-            </li>
-
-            <li>
-
-                <img src="./views/img/banner04.jpg" alt="img" class="img-fluid">
-
-            </li>
-
-        </ul>
-
-    </div>
-
-    <div class="controller d-none">
-
-        <a class="auto" href="#">
-
-            <i class="fas fa-play fa-xs"></i>
-            <i class="fas fa-pause fa-xs"></i>
-
-        </a>
-
-        <div class="indicate-area"></div>
-
-    </div>
-
-</section>
 
 <!--=====================================
 MENU
@@ -65,43 +12,43 @@ MENU
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Suramérica</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Suramérica</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Centroamérica</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Centroamérica</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Norteamérica</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Norteamérica</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Europa</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Europa</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Asia</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Asia</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Africa</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Africa</a>
 
         </li>
 
         <li class="nav-item">
 
-            <a class="nav-link text-white" href="categorias.html">Mi viaje por Oceanía</a>
+            <a class="nav-link text-white" href="../categorias.html">Mi viaje por Oceanía</a>
 
         </li>
 
